35bb2fa3d389bcb274c0ff2bfce36f439502a98d
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-09 19:35:37 +0000

    implemented the random string generator to create new short links, which is saved to urlsdatabase object; postroute now redirects to newly created page with link; /u/:id extension now redirects to longURL pages

--- express_server.js
+++ express_server.js
@@ -32,8 +32,9 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); 
-  res.send("Ok"); 
+  let x = generateRandomString();
+  urlDatabase[x] = req.body.longURL;
+  res.redirect(`/urls/${x}`); 
 });
 
 app.get("/urls/new", (req, res) => {
@@ -43,6 +44,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.get("/hello", (req, res) => {
